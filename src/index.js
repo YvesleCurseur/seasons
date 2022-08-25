ff46158f1250e7d4a8e313c6933271074867dd8b
YvesleCurseur <pognonyvesfulbert@gmail.com>
2022-08-25 17:45:22 +0000

    Show latitude error and loading...

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM  from "react-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Refactor the component
+class App extends React.Component{
+    //Fonction de javascript
+    constructor(props){
+        //super pour appeller les éléments dans le constructor qui lui se trouve dans le Component
+        super(props)
+        //initialiser un state (Manière plus clean de le faire)
+        this.state = {lat: null, errorMessage: ''}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        // Récupérer la position de l'utilisateur grâce une API de mozilla developper 
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                //Pour mettre à jour notre state on utiliste setState
+                this.setState({lat: position.coords.latitude})
+            },
+            (err) => {
+                this.setState({errorMessage: err.message})
+            }
+        )
+    }
+
+    render(){
+        //Mettre un state dans le jsx
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error, {this.state.errorMessage}</div>
+        }
+        
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latidute is {this.state.lat}</div>
+        }
+        
+        return <div>Loading...</div>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
